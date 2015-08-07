cbc4e1098c3c083fc30a6fb0b8bfaade6a75ef99
lisaike <lisaikeha@gmail.com>
2015-08-07 16:05:37 +0800

    Initial Commit

---
+++ EventHandleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EventHandleDemo
+//
+//  Created by lisaike on 15/8/7.
+//  Copyright (c) 2015年 lisaike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EventHandleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EventHandleDemo
+//
+//  Created by lisaike on 15/8/7.
+//  Copyright (c) 2015年 lisaike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
